d8b008cca8c4d2addf95ebfbe3dced070179da8c
yatra-tank <yatra.tank.cg@gmail.com>
2026-08-23 15:36:06 +0530

    python.py file added

---
+++ 02-variables/python.py
@@ -0,0 +1,27 @@
+name = "Yatra Tank"
+age = 17
+city = "Jamnagar"
+print("Name: ", name, "\n", "Age: ", age, "\n", "City: ", city)
+
+student_name = "Yatra Tank"
+student_rollno = 112387
+student_branch = "CSE AI"
+print("Student Name: ", student_name, "\n", "Student Roll Number: ", student_rollno,"\n", "Student Branch: ", student_branch)
+
+marks = 32
+marks = 40
+print(marks)
+# itll print 40, as the value is now reassigned
+
+name, age, city = "Rahul", 18, "Patna"
+x = y = z= 0
+
+student1 = "Rahul"
+student_name = "Rahul"
+student_class = "BTech"
+
+student_name = "Yatra"
+age = 17
+age = 18
+print("Student Name: ", student_name, "Student Age: ", age)
+# python reads the code from top to bottom and hence the age(17) is reassigned as 18 as the code proceeds further
