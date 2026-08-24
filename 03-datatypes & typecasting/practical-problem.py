2c6824bc272f838c0555ebc6a19249687ed36ea2
yatra-tank <yatra.tank.cg@gmail.com>
2026-08-24 16:49:59 +0530

    practical-problem.py file added

---
+++ 03-datatypes & typecasting/practical-problem.py
@@ -0,0 +1,39 @@
+student_name = "Yatra Tank"
+student_age = 17
+student_height = 169.79
+student_attendence = True
+student_result = None
+print(student_name, type(student_name)) #str
+print(student_age, type(student_age)) #int
+print(student_height, type(student_height)) #float
+print(student_attendence, type(student_attendence)) #bool
+print(student_result, type(student_result)) #NoneType
+
+a = 50
+b = 50.0
+c = "50"
+print(type(a))
+print(type(b))
+print(type(c))
+
+a = True
+b = "True"
+print(type(a))
+print(type(b))
+
+value = 17
+print(type(value))
+value = "string"
+print(type(value))
+# the datatype changed to string
+
+product_name = "FaceWash"
+product_qty = 2
+product_price = 150.00
+product_availabilty = True
+product_discount = None
+print(type(product_name))
+print(type(product_qty))
+print(type(product_price))
+print(type(product_availabilty))
+print(type(product_discount))
