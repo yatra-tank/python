3b84e90e9487c6929718b504e6ee20d27d80fe64
yatra-tank <yatra.tank.cg@gmail.com>
2026-08-24 11:36:21 +0530

    datatype and typecasting file modified

--- 03-datatypes & typecasting/python.py
+++ 03-datatypes & typecasting/python.py
@@ -1,10 +1,23 @@
-age = 17 #int
-Age = 17.8 #float
-aGE = "17" #str
-agE = True #bool
-aGe = None #NoneType
-print(type(age))
-print(type(Age))
-print(type(aGE))
-print(type(agE))
-print(type(aGe))
+# age = 17 #int
+# Age = 17.8 #float
+# aGE = "17" #str
+# agE = True #bool
+# aGe = None #NoneType
+# print(type(age))
+# print(type(Age))
+# print(type(aGE))
+# print(type(agE))
+# print(type(aGe))
+
+number_text = "25"
+number = int(number_text)
+print(number, type(number)) #int
+print(number_text, type(number_text)) #string
+
+price = 99.8
+whole_price = int(price) 
+print(whole_price, type(whole_price)) #int
+
+marks = 85
+marks_text = str(marks)
+print(marks_text, type(marks_text)) #str
