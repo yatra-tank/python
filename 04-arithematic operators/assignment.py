cfa10f04d56c2135704258522560f90ce1a2b82c
yatra-tank <yatra.tank.cg@gmail.com>
2026-08-26 10:05:47 +0530

    assignment.py added

---
+++ 04-arithematic operators/assignment.py
@@ -0,0 +1,94 @@
+print("QUESTION1")
+a = 3
+b = 2
+print("addition: ", a+b)
+print("subtraction: ", a-b)
+print("multiplication: ", a*b)
+print("division: ", a/b)
+print("floor division: ", a//b)
+print("modulus: ", a%b)
+print("exponentiation: ", a**b)
+
+
+print("QUESTION2")
+a = 2
+b = 5.0
+print("addition: ", a+b, type(a+b))
+print("subtraction: ", a-b, type(a-b))
+print("multiplication: ", a*b, type(a*b))
+print("division: ", a/b, type(a/b))
+print("floor division: ", a//b, type(a//b))
+print("modulus: ", a%b, type(a%b))
+print("exponentiation: ", a**b, type(a**b))
+
+print("QUESTION3")
+subject1 = 40
+subject2 = 38
+subject3 = 36
+print("Total Marks: ", subject1 + subject2 + subject3)
+print("Average Marks: ", (subject1 + subject2 + subject3)/3)
+
+print("QUESTION4")
+product_price = 200
+qty = 3
+print("Total Price: ", product_price*3)
+
+print("QUESTION5")
+i = int(input("Enter the integer: "))
+if i%2 == 0:
+    print("Even number")
+else:
+    print("Odd Number")
+
+print("QUESTION6")
+a = 2
+b = 6
+print("Division: ", a/b)
+print("Floor Division: ", a//b)
+
+print("QUESTION7")
+a = -5
+b = -3
+print("addition: ", a+b)
+print("subtraction: ", a-b)
+print("multiplication: ", a*b)
+print("division: ", a/b)
+print("floor division: ", a//b)
+print("modulus: ", a%b)
+
+print("QUESTION8")
+a = 12
+b = 13
+c = -2
+d = -5
+print("positive - positive: ", a - b)
+print("positive - negative: ", a - c)
+print("negative - positive: ", c - a)
+print("negative - negative: ", d - c)
+
+print("QUESTION9")
+a = 12
+b = 13
+c = -2
+d = -5
+print("positive // positive: ", a // b)
+print("negative // positive: ", a // c)
+print("positive // negative: ", c // a)
+print("negative // negative: ", d // c)
+
+print("QUESTION10")
+a = 12
+b = 13
+c = -2
+d = -5
+print("positive % positive: ", a % b)
+print("negative % positive: ", a % c)
+print("positive % negative: ", c % a)
+print("negative % negative: ", d % c)
+
+print("QUESTION11")
+a = Tru
+print("positive % positive: ", a % b)
+print("negative % positive: ", a % c)
+print("positive % negative: ", c % a)
+print("negative % negative: ", d % c)
