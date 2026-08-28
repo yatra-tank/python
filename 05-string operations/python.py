4fd7290a9227ff82408e03ca2a0a0156fd273de2
yatra-tank <yatra.tank.cg@gmail.com>
2026-08-28 18:56:31 +0530

    string operations added

---
+++ 05-string operations/python.py
@@ -0,0 +1,10 @@
+word = "Python"
+print(word[:3].upper() + word[3:].lower() )
+
+a = " pYTHON"
+print(a.capitalize())
+
+a = """pYTHON ∴ ⊍ 🀃	𞲖	𞲠𞲡	𞲢𞲣𞲤	𞲥	𞲦	𞲧 
+    ä, ö, ü, Ä, Ö, Ü µ ﬁ ﬄ
+    """
+print(a.casefold())
