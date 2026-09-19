93c374dda0c33967440b159158f7028e2e06c0b5
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-19 11:28:23 +0530

    electricity bill updated

--- 09-problem solving/02/16-electricity-bill.py
+++ 09-problem solving/02/16-electricity-bill.py
@@ -1,13 +1,13 @@
 units = float(input("Enter electricity units: "))
 
 if units < 0:
-    print("Invalid units")
+    print("invalid units")
 elif units <= 100:
     bill = units * 5
-    print("Electricity bill =", bill)
+    print("electricity bill =", bill)
 elif units <= 200:
     bill = 100 * 5 + (units - 100) * 7
-    print("Electricity bill =", bill)
+    print("electricity bill =", bill)
 else:
     bill = 100 * 5 + 100 * 7 + (units - 200) * 10
-    print("Electricity bill =", bill)
+    print("electricity bill =", bill)
