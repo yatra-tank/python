ef1e238d1aa494dd42043e4d704c742d4f608445
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-25 00:26:33 +0530

    answers added

--- 09-problem solving/02-fifty-questions/01/10-number-pattern-conditions.py
+++ 09-problem solving/02-fifty-questions/01/10-number-pattern-conditions.py
@@ -1,7 +1,7 @@
-n = int(input("Enter n: "))
-for i in range(1, n + 1):
+n = int(input("Enter your number: "))
+for i in range(1, 2 *n ):
     for j in range(1, i + 1):
-        if j % 15 == 0:
+        if j % 3 == 0 and j % 5 == 0:
             print("Z", end=" ")
         elif j % 3 == 0:
             print("X", end=" ")
