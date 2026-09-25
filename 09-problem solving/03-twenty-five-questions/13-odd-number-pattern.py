7b872f8880ad3a64e0c72e8ccfac1a1b8dccd319
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-25 11:42:39 +0530

    alternate answer added

--- 09-problem solving/03-twenty-five-questions/13-odd-number-pattern.py
+++ 09-problem solving/03-twenty-five-questions/13-odd-number-pattern.py
@@ -1,5 +1,12 @@
+# n = int(input("Enter your number: "))
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print(2 * j - 1, end=" ")
+#     print("")
+
 n = int(input("Enter your number: "))
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(2 * j - 1, end=" ")
-    print("")
+for i in range(n):
+    for j in range(2 * i + 2):
+        if j%2 != 0:
+            print(j, end=" ")
+    print()
